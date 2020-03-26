d3a27a3e40bd7f2a5d636f7e49edf4a2a3b4351f
Shivu Mahamani <Shivu.mahamani@gmail.com>
2020-03-26 16:40:25 +0900

    update config

--- conf/config.py
+++ conf/config.py
@@ -25,15 +25,15 @@ DEFAULT_LANG = 'ja'
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@xxx.com"
-DOMAIN_ID = 0
+ADMIN_ACCOUNT = "shivu.mahamani@gmail.com"
+DOMAIN_ID = 10175504
 
 # api
-API_ID = "kr1EHxxxxfJVz"
-SERVER_CONSUMER_KEY = "To8SnC7sxxxxxjqXZhO"
+API_ID = "jp1hipIEBeDUo"
+SERVER_CONSUMER_KEY = "uLKJLanbaiLODRd8M0s1"
 
-SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
-SECRET_KEY_NAME = "private_201xxxxx164308.key"
+SERVER_ID = "24e72399c0214acb9eac5fc6d357f52b"
+SECRET_KEY_NAME = "private_20200326162837.key"
 
 # LOCAL ADDRESS
 LOCAL_ADDRESS = "https://[Heroku app name].herokuapp.com/"
